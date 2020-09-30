dcbb491526b67c9f9ea00065ce7dfeb09351297e
Dima-Iv <dimaivanenko99@gmail.com>
2020-09-30 16:40:06 +0300

    Finished protected NoteBook

--- src/client_info.py
+++ src/client_info.py
@@ -1,14 +1,23 @@
 import os
 import time
-from socket import socket
-
 import rsa
+from socket import socket
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util.Padding import pad, unpad
+
+
+def encrypt_with_RSA(msg: bytes, client_public_key):
+    ans = b''
+    m = (ClientInfo.KEY_LEN // 8) - 11
+    n = len(msg) // m
+    for i in range(n):
+        ans += rsa.encrypt(msg[i * m:(i + 1) * m], client_public_key)
+    return ans + rsa.encrypt(msg[n * m:], client_public_key)
 
 
 class ClientInfo:
-    dict_for_login_and_pass = None
     KEY_LEN = 1024
-    PATH_TO_LOGIN_PASSWORD = 'clients_files/users_passwords.txt'
 
     def __init__(self, connection: socket, address):
         self.connection = connection
@@ -16,92 +25,96 @@ class ClientInfo:
         self.login = ''
         self.password = ''
         self.client_public_RSA_key = None
-        self.server_keys_chain = None
+        self.session_key = None
         self.key_time = 0
 
     @property
-    def final_path_to_dir(self):
+    def final_path_to_user_dir(self):
         return 'clients_files/' + self.login
 
-    @staticmethod
-    def decrypt(msg: bytes, serv_private_key):
-        ans = b''
-        m = ClientInfo.KEY_LEN // 8
-        n = len(msg) // m
-        for i in range(n):
-            ans += rsa.decrypt(msg[i * m:(i + 1) * m], serv_private_key)
-        return ans
+    def encrypt_message(self, message: str) -> bytes:
+        iv = Random.get_random_bytes(16)
+        cipher = AES.new(self.session_key, AES.MODE_CBC, iv)
+        cipher_text = iv + cipher.encrypt(pad(message.encode(), AES.block_size))
+        return cipher_text
 
-    @staticmethod
-    def encrypt(msg: bytes, client_public_key):
-        ans = b''
-        m = (ClientInfo.KEY_LEN // 8) - 11
-        n = len(msg) // m
-        for i in range(n):
-            ans += rsa.encrypt(msg[i * m:(i + 1) * m], client_public_key)
-        return ans + rsa.encrypt(msg[n * m:], client_public_key)
+    def decrypt_message(self, message: bytes) -> str:
+        iv = message[:16]
+        cipher = AES.new(self.session_key, AES.MODE_CBC, iv)
+        decrypt_text = unpad(cipher.decrypt(message[16:]), AES.block_size)
+        return decrypt_text.decode()
 
-    def decrypt_me(self, message: bytes) -> str:
-        return self.decrypt(message, self.server_keys_chain[1]).decode()
+    def encrypt_file_data(self, data: str, password=None) -> bytes:
+        if password is None:
+            password = self.password
+        byte_key_from_password = password.__hash__().to_bytes(32, byteorder='big', signed=True)
+        iv = Random.get_random_bytes(16)
+        cipher = AES.new(byte_key_from_password, AES.MODE_CBC, iv)
+        cipher_text = iv + cipher.encrypt(pad(data.encode(), AES.block_size))
+        return cipher_text
 
-    def encrypt_me(self, message: bytes):
-        return self.encrypt(message, self.client_public_RSA_key)
+    def decrypt_file_data(self, data: bytes, password=None) -> str:
+        if password is None:
+            password = self.password
+        iv = data[:16]
+        byte_key_from_password = password.__hash__().to_bytes(32, byteorder='big', signed=True)
+        cipher = AES.new(byte_key_from_password, AES.MODE_CBC, iv)
+        decrypt_text = unpad(cipher.decrypt(data[16:]), AES.block_size)
+        return decrypt_text.decode()
 
-    def registration(self, message: str):
-        print(message)
-        current_login, current_password = message.split('\n')
-        if self.dict_for_login_and_pass.get(current_login, None) is not None:
-            self.connection.send(self.encrypt('This login is occupied'.encode(), self.client_public_RSA_key))
+    def registration(self, login_and_password: str):
+        print(login_and_password)
+        current_login, current_password = login_and_password.split('\n')
+        if os.path.exists('clients_files/' + current_login):
+            self.connection.send(self.encrypt_message('This login is occupied'))
         else:
-            self.dict_for_login_and_pass[current_login] = current_password
             os.mkdir('clients_files/' + current_login)
-            with open(self.PATH_TO_LOGIN_PASSWORD, 'a') as f:
-                f.write(current_login + '\t' + current_password + '\n')
-            self.connection.send(self.encrypt('OK'.encode(), self.client_public_RSA_key))
+            with open('clients_files/' + current_login + '/password.txt', 'wb') as f:
+                f.write(self.encrypt_file_data(current_password, password=current_password))
+            self.connection.send(self.encrypt_message('OK'))
 
-    def log_in(self, message: str):
-        print(message)
-        current_login, current_password = message.split('\n')
-        true_password = self.dict_for_login_and_pass.get(current_login, None)
-        if true_password is None:
-            self.connection.send(self.encrypt('This login is not exist'.encode(), self.client_public_RSA_key))
-        elif current_password == true_password:
-            self.login = current_login
-            self.password = current_password
-            self.connection.send(self.encrypt('OK'.encode(), self.client_public_RSA_key))
+    def log_in(self, login_and_password: str):
+        print(login_and_password)
+        current_login, current_password = login_and_password.split('\n')
+        if os.path.exists('clients_files/' + current_login):
+            with open('clients_files/' + current_login + '/password.txt', 'rb') as f:
+                true_password = self.decrypt_file_data(f.read(), password=current_password)
+            if current_password == true_password:
+                self.login = current_login
+                self.password = current_password
+                self.connection.send(self.encrypt_message('OK'))
+            else:
+                self.connection.send(self.encrypt_message('Invalid password'))
         else:
-            self.connection.send(self.encrypt('Invalid password'.encode(), self.client_public_RSA_key))
+            self.connection.send(self.encrypt_message('This login is not exist'))
 
     def get_notebook_list(self):
-        files_list = '\n'.join(os.listdir(self.final_path_to_dir))
-        self.connection.send(self.encrypt(files_list.encode(), self.client_public_RSA_key))
+        files_list = '\n'.join(os.listdir(self.final_path_to_user_dir)[1:])
+        self.connection.send(self.encrypt_message(files_list))
 
-    def send_key(self, message: bytes):
-        print(message)
+    def send_key(self, public_client_key: bytes):
+        print(public_client_key)
         self.key_time = time.time()
-        self.client_public_RSA_key = rsa.PublicKey.load_pkcs1(message)
-        self.server_keys_chain = rsa.newkeys(self.KEY_LEN)
-        self.connection.send(self.encrypt(self.server_keys_chain[0].save_pkcs1(), self.client_public_RSA_key))
+        self.client_public_RSA_key = rsa.PublicKey.load_pkcs1(public_client_key)
+        self.session_key = Random.get_random_bytes(32)
+        self.connection.send(encrypt_with_RSA(self.session_key, self.client_public_RSA_key))
 
-    def open_file(self, message: str):
-        print(message)
-        finish_path_to_file = self.final_path_to_dir + '/' + message
-        with open(finish_path_to_file, 'r') as f:
-            self.connection.send(self.encrypt(f.read().encode(), self.client_public_RSA_key))
+    def open_file(self, file_name: str):
+        print(file_name)
+        finish_path_to_file = self.final_path_to_user_dir + '/' + file_name
+        with open(finish_path_to_file, 'rb') as file:
+            self.connection.send(self.encrypt_message(self.decrypt_file_data(file.read())))
 
-    def save_file(self, message: str):
-        print(message)
-        file_name, data = message.split('\n', 1)
-        finish_path_to_file = self.final_path_to_dir + '/' + file_name
-        with open(finish_path_to_file, 'w') as f:
-            for b in data.encode():
-                print(b)
-            print(self.password.__hash__())
-            f.write(data)
-            self.connection.send(self.encrypt('Saved'.encode(), self.client_public_RSA_key))
+    def save_file(self, file_name_and_data: str):
+        print(file_name_and_data)
+        file_name, data = file_name_and_data.split('\n', 1)
+        finish_path_to_file = self.final_path_to_user_dir + '/' + file_name
+        with open(finish_path_to_file, 'wb') as file:
+            file.write(self.encrypt_file_data(data))
+            self.connection.send(self.encrypt_message('Saved'))
 
-    def delete_file(self, message: str):
-        print(message)
-        finish_path_to_file = self.final_path_to_dir + '/' + message
+    def delete_file(self, file_name: str):
+        print(file_name)
+        finish_path_to_file = self.final_path_to_user_dir + '/' + file_name
         os.remove(finish_path_to_file)
-        self.connection.send(self.encrypt_me('Deleted'.encode()))
+        self.connection.send(self.encrypt_message('Deleted'))
--- src/runner.py
+++ src/runner.py
@@ -5,8 +5,8 @@ from src.client_info import ClientInfo
 
 
 class Runner(Thread):
-    max_len_of_data = 1000000
-    max_key_time = 40
+    MAX_LEN_OF_DATA = 1000000
+    MAX_KEY_TIME = 40
 
     def __init__(self, client_info: ClientInfo):
         super().__init__()
@@ -15,26 +15,26 @@ class Runner(Thread):
     def run(self):
         try:
             while True:
-                encrypted_message = self.client_info.connection.recv(self.max_len_of_data)
+                encrypted_message = self.client_info.connection.recv(self.MAX_LEN_OF_DATA)
                 print(encrypted_message)
                 if encrypted_message[0:1] == b'k':
                     self.client_info.send_key(encrypted_message[1:])
-                elif time.time() - self.client_info.key_time >= self.max_key_time:
-                    self.client_info.connection.send(self.client_info.encrypt_me('key expired'.encode()))
+                elif time.time() - self.client_info.key_time >= self.MAX_KEY_TIME:
+                    self.client_info.connection.send(self.client_info.encrypt_message('key expired'))
                 elif encrypted_message[0:1] == b'l':
-                    self.client_info.log_in(self.client_info.decrypt_me(encrypted_message[1:]))
+                    self.client_info.log_in(self.client_info.decrypt_message(encrypted_message[1:]))
                 elif encrypted_message[0:1] == b'r':
-                    self.client_info.registration(self.client_info.decrypt_me(encrypted_message[1:]))
+                    self.client_info.registration(self.client_info.decrypt_message(encrypted_message[1:]))
                 elif self.client_info.login == '':
-                    self.client_info.connection.send(self.client_info.encrypt_me('Error: You are not login'.encode()))
+                    self.client_info.connection.send(self.client_info.encrypt_message('Error: You are not login'))
                 elif encrypted_message[0:1] == b'g':
                     self.client_info.get_notebook_list()
                 elif encrypted_message[0:1] == b'o':
-                    self.client_info.open_file(self.client_info.decrypt_me(encrypted_message[1:]))
+                    self.client_info.open_file(self.client_info.decrypt_message(encrypted_message[1:]))
                 elif encrypted_message[0:1] == b's':
-                    self.client_info.save_file(self.client_info.decrypt_me(encrypted_message[1:]))
+                    self.client_info.save_file(self.client_info.decrypt_message(encrypted_message[1:]))
                 elif encrypted_message[0:1] == b'd':
-                    self.client_info.delete_file(self.client_info.decrypt_me(encrypted_message[1:]))
+                    self.client_info.delete_file(self.client_info.decrypt_message(encrypted_message[1:]))
                 elif encrypted_message == b'':
                     self.client_info.connection.close()
                     return
